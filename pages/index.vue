6e445d30dc4c65af5417bcbf0970696e30741569
JoaoMRL <ribeiroleite.joaomarco@gmail.com>
2023-07-24 10:44:21 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup> 
+const first = ref('test');
+
+
+function changeCa() {
+  first.value ='Prout';
+}
+</script>
+
+<template>
+  <div>
+    {{ first }}
+    
+  </div>
+  <button @click="changeCa()">Ne cliquez pas !</button>
+</template>
+
+<style scoped></style>
---
+++ pages/name-list.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const list:Ref<string[]> = ref(['Billy','Bill','Bob','DIO']);
+const newName:Ref<string>=ref('');
+
+
+function addName() {
+  list.value.push(newName.value);
+}
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="newName">
+    <button @click="addName()">Ajouter au tableau</button>
+  </div>
+  <div>
+    <ul>
+      <li v-for="item of list">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
