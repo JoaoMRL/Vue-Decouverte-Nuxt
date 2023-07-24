9ce0305245fa0e79f5caf8cfb8e2a406d85fc78e
JoaoMRL <ribeiroleite.joaomarco@gmail.com>
2023-07-24 16:44:59 +0200

    Output

---
+++ components/form-dog.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { Dog } from '../entities';
+import {Ref} from 'vue';
+
+const wof:Ref<Dog> =ref<Dog>({name:'',breed:'',birthdate:''});
+  // const emit = defineEmits=(['submitted']);
+const emit = defineEmits<{
+  (e:'submitted',wof:Dog):void
+}>();
+
+
+function handleSubmit() {
+  emit('submitted',wof.value);
+}
+</script>
+
+<template>
+  <div>
+    <form action="" method="post" @submit.prevent="handleSubmit()">
+      <label for="name">name</label>
+      <input type="text" id="name" placeholder="name" v-model="wof.name">
+      <label for="breed">breed</label>
+      <input type="text" id="breed" placeholder="breed"  v-model="wof.breed">
+      <label for="birthdate">birthdate</label>
+      <input type="date" id="birthdate" placeholder="birthdate"  v-model="wof.birthdate">
+      <button >Submit</button>
+    </form>
+  </div>
+</template>
+
+<style scoped></style>
--- pages/list-dog.vue
+++ pages/list-dog.vue
@@ -1,16 +1,27 @@
 <script lang="ts" setup>
 import { Dog } from 'entities';
-const { data: count } = await useFetch<Dog>('http://localhost:8000/api/dog')
+const { data: count,refresh } = await useFetch<Dog>('http://localhost:8000/api/dog')
 
+async function addDog(dog:Dog){
+  await $fetch('http://localhost:8000/api/dog',{
+    method:'POST',
+    body:dog
+  });
+  refresh();
+}
 
 </script>
 
 <template>
   <div>
+    <FormDog @submitted="addDog($event)" />
+  </div>
+  <div>
     <p v-for="item of count">
       <DogItem :doggy="item" />
     </p>
   </div>
+  
 </template>
 
 <style scoped></style>
